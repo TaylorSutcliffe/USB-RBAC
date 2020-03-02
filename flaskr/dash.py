92524e4008811a0c07457be03a7b14d81d00de5e
Taylor <t.sutcliffe@ncl.ac.uk>
2020-03-02 16:11:14 +0000

    Url endpoints added for each room

--- flaskr/dash.py
+++ flaskr/dash.py
@@ -6,5 +6,15 @@ from werkzeug.exceptions import abort
 bp = Blueprint('dash', __name__)
 
 @bp.route('/')
-def index():
-    return render_template('dash/index.html')
+def redr():
+    return redirect(url_for('dash.index', room = 'G.062'))
+
+@bp.route('/<string:room>/')
+def index(room):
+    #check if user has permission to be on room page
+
+    #if so generate appropriate visulisation 
+
+    #add variable to return with the visulisations and then
+    #modify the index.html to display this 
+    return render_template('dash/index.html', room=room)
