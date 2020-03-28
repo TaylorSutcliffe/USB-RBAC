8241ae4fbbc6f320b9e6b30d82e0cc72e633b427
Aditya Vaidya <adityamv79@gmail.com>
2020-03-28 11:50:26 +0000

    Update lightlevel_3015.py

--- flaskr/lightlevel_3015.py
+++ flaskr/lightlevel_3015.py
@@ -1,5 +1,5 @@
 import requests
-import simplejson as json
+
 
 def lightlevel_3015():
     data = requests.get('https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity?meta:roomNumber=3.015&metric="BrightnessValue"')
@@ -7,7 +7,7 @@ def lightlevel_3015():
     #print(obj['items'][0]['feed'][0]['timeseries'][0]['timeseriesId'])
     timeseriesid = str(obj['items'][0]['feed'][0]['timeseries'][0]['timeseriesId'])
 
-    url = 'https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/'+timeseriesid+'/historic?startTime=2020-02-20T13:00:00Z&endTime=2020-02-27T13:00:00Z'
+    url = 'https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/'+timeseriesid+'/historic?startTime=2020-03-20T13:00:00Z&endTime=2020-03-27T13:00:00Z'
     data2 = requests.get(url)
     obj2 = data2.json()
     obj3 = obj2['historic']['values']
