68f526259b328bb4d148bbcbe2565bc4d0b0806b
Taylor <t.sutcliffe@ncl.ac.uk>
2020-04-03 11:46:12 +0100

    Time changed to march for data clarity

--- flaskr/atrium_temperature.py
+++ flaskr/atrium_temperature.py
@@ -8,7 +8,7 @@ def atrium_temperature():
     #print(obj['items'][0]['feed'][0]['timeseries'][0]['timeseriesId'])
     timeseriesid = str(obj['items'][0]['feed'][0]['timeseries'][0]['timeseriesId'])
 
-    url = 'https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/'+timeseriesid+'/historic?startTime=2020-03-20T13:00:00Z&endTime=2020-03-27T13:00:00Z'
+    url = 'https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/timeseries/'+timeseriesid+'/historic?startTime=2020-02-20T01:00:00Z&endTime=2020-02-27T01:00:00Z'
     data2 = requests.get(url)
     obj2 = data2.json()
     obj3 = obj2['historic']['values']
